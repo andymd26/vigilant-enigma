039bab33571dd603737592e471eaf8b43b8ab4c3
andymd26 <andymd26@gmail.com>
2016-11-02 21:17:24 -0400

    Created the monte carlo code.

--- src/montecarlo.r
+++ src/montecarlo.r
@@ -1,19 +1,27 @@
-threshold = 2020
-x = c(6, 6, 5, 4, 4)
+require(dplyr)
+
+threshold = 65000
+x = c(100, 150, 200, 25, 30)
 # [Integer variable] the number of each unit type picked by the 1st stage of the model
-avg.size = c(50, 100, 100, 400, 1000)
+avg.size = c(50, 50, 100, 500, 1000)
 # the average size of each unit type in the model
 avg.for = c(0.6, 0.85, 0.85, 0.9, 0.95)
 # the average forced outage rate of each unit type in the model
 for.rep = c(rep(avg.for[1], x[1]), rep(avg.for[2], x[2]), rep(avg.for[3], x[3]), rep(avg.for[4], x[4]), rep(avg.for[5], x[5]))
-size = c(rep(avg.size[1], x[1]), rep(avg.size[2], x[2]), rep(avg.size[3], x[3]), rep(avg.size[4], x[4]), rep(avg.size[5], x[5]))
-x.on = diag(c(rep(avg.for[1], x[1]), rep(avg.for[2], x[2]), rep(avg.for[3], x[3]), rep(avg.for[4], x[4]), rep(avg.for[5], x[5])))
-x.off = diag(c(1-diag(x.on)))
-x.size = diag(c(size))
-x.tot = sum(x)
+size.rep = matrix(c(rep(avg.size[1], x[1]), rep(avg.size[2], x[2]), rep(avg.size[3], x[3]), rep(avg.size[4], x[4]), rep(avg.size[5], x[5])), nrow = 1)
 
-x = c
-
-y = t(sapply(for.rep, function(x) rbinom(n=1000, size=1, prob =x)))
+y = t(sapply(for.rep, function(x) rbinom(n=100000, size=1, prob =x)))
+x = data.frame(mw = c(size.rep %*% y)) %>%
+  group_by(mw) %>%
+  summarize(freq=n()) %>%
+  mutate(rel.freq = freq/sum(freq)) %>%
+  arrange(desc(mw)) %>%
+  # Arrange in descending order (for the cumulative probability calculation)
+  mutate(cum.prob = cumsum(rel.freq)) %>%
+  # Calculate the cumulative probability of each state (i.e., Prob(X >= x mw))
+  filter(mw >= threshold) %>%
+  # The user specifies a capacity threshold that corresponds with a probability of outage (calculated from the load duration curve)
+  # Identify the states with equivalent or greater capacity
+  filter(cum.prob == max(cum.prob))
 
 
