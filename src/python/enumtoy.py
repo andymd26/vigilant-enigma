d852ff9a78bf724f6cccfe1ec38c9309fcadddfa
David Prentiss <dprentiss@gmail.com>
2017-02-03 14:05:20 -0500

    comment outline

--- src/python/enumtoy.py
+++ src/python/enumtoy.py
@@ -1,6 +1,6 @@
 budget = 1000
 aveCap = 0.5 # lambda 
-scenarioProb = [0.25, 0.5, 0.25] # prob_s, probability of each scenario
+# scenarioProb = [0.25, 0.5, 0.25] # prob_s, probability of each scenario
 
 # *_p technology parameters
 technologies = [
@@ -16,5 +16,13 @@ technologies = [
      }
     ]
 
+# Variables
+maxObjective = 0
 
-
+#Store 0 as max objective value
+#loop over all possible well-formed solutions
+    #Check feasibility of current solution
+        #If not feasible, next salution
+    #Evaluate objective value for current solution
+        #If value is higher than stored value, store current value and solution
+#Return solution and objective value
