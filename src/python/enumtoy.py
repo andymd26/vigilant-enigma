53f44f6c79ba874fa0178ef8e2106e024cbc5a84
David Prentiss <dprentiss@gmail.com>
2017-02-05 19:59:56 -0500

    vars and params

--- src/python/enumtoy.py
+++ src/python/enumtoy.py
@@ -1,7 +1,7 @@
 budget = 1000
-aveCap = 0.5 # lambda 
-N = 1 # number of nodes
-# scenarioProb = [0.25, 0.5, 0.25] # prob_s, probability of each scenario
+aveCap = 0.5 # lambda
+H = 1
+
 
 # *_p technology parameters
 P = [
@@ -20,23 +20,31 @@ P = [
 S = [
     {"prob": [[0, 0], [0, 0], [0, 0]],
      "demand": [[0, 0], [0, 0], [0, 0]]
-     },
-    {"prob": [[0, 0], [0, 0], [0, 0]],
-     "demand": [[0, 0], [0, 0], [0, 0]]
-     },
-    {"prob": [[0, 0], [0, 0], [0, 0]],
-     "demand": [[0, 0], [0, 0], [0, 0]]
      }
     ]
 
-# Variables
+### Variables
+
 minObj = float("inf") #Stores the smallest know objective function value, initialised to INF
 
 # Number of each type of each power production technology p to build at node n, integer
-xnp = [[0 for j in xrange(len(P))] for i in xrange(len(N))]
+mhns = [[[0 for k in range(len(S))] for j in range(len(S))] for i in range(3)]
+
+# Number of each type of each power production technology p to build at node n, integer
+uehns = [[0 for j in range(len(P))] for i in range(len(S))]
+
+# Number of each type of each power production technology p to build at node n, integer
+xnp = [[0 for j in range(len(P))] for i in range(len(S))]
+
+# Number of each type of each power production technology p to build at node n, integer
+yhnsp = [[0 for j in range(len(P))] for i in range(len(S))]
 
 #loop over all possible well-formed solutions
     #If not feasible, next salution
     #Evaluate objective value for current solution
+    
         #If value is smaller than stored value, store current value and solution
 #Return solution and objective value
+
+def evalObj(xnp):
+    print(xnp)
