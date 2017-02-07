5159aa3d2f63553eaa1ee0d8210859ff68d31da3
David Prentiss <dprentiss@gmail.com>
2017-02-07 11:54:25 -0500

    pause python and init gams enum toy problem
    I realized that the problem is mixed integer, so there is no stritly
    enumerative approach. This GAMS implementation will enumerate over the
    discrete investment choices and solve the linear operation problem.

---
+++ src/GAMS/enumtoygep.gms
@@ -0,0 +1,40 @@
+Sets
+p technology /p1*p2/
+;
+Variables
+z;
+Positive Variables
+x(p),y(p)
+;
+Parameters
+size(p) 'average size of plant t in MW'
+  /p1 100
+   p2 200 /
+
+k(p) 'fixed cost of plant t in USD per MW'
+  /p1 10
+   p2 7 /
+
+c(p) 'marginal cost of plant t in USD per MWh'
+  /p1 4
+   p2 4.5 /
+r(p)
+  /p1 1
+   p2 1 /
+
+d 'demand'
+  /10.6/
+;
+
+Equations
+obj
+demand
+pr(p)
+;
+obj.. z =e= sum(p, x(p)*k(p) + y(p)*c(p));
+demand.. sum(p, y(p)) =g= d ;
+pr(p).. y(p) =l= x(p)*r(p) ;
+
+model toy_gep /all/;
+solve toy_gep using lp minimizing z;
+
--- src/python/enumtoy.py
+++ src/python/enumtoy.py
@@ -1,6 +1,9 @@
 budget = 1000
-aveCap = 0.5 # lambda
-H = 1
+aveCap = 4365 # lambda (MW)
+H = 2
+S = 1
+LOLCC = 0.95
+VLL = 1000 # ($/MW)
 
 
 # *_p technology parameters
@@ -17,9 +20,9 @@ P = [
      }
     ]
 
-S = [
-    {"prob": [[0, 0], [0, 0], [0, 0]],
-     "demand": [[0, 0], [0, 0], [0, 0]]
+N = [
+    {"prob": [[0], [0]],
+     "demand": [[0], [0]]
      }
     ]
 
@@ -27,17 +30,22 @@ S = [
 
 minObj = float("inf") #Stores the smallest know objective function value, initialised to INF
 
-# Number of each type of each power production technology p to build at node n, integer
-mhns = [[[0 for k in range(len(S))] for j in range(len(S))] for i in range(3)]
+# Import or export of power to or from node n for operating mode h under scenario s
+# (MWh)
+mhns = [[[0 for k in range(S)] for j in range(len(N))] for i in range(H)]
 
-# Number of each type of each power production technology p to build at node n, integer
-uehns = [[0 for j in range(len(P))] for i in range(len(S))]
+# Unserved energy at node n during operating level h for scenario s
+#(MWh)
+uehns = [[[0 for k in range(S)] for j in range(len(N))] for i in range(H)]
 
-# Number of each type of each power production technology p to build at node n, integer
-xnp = [[0 for j in range(len(P))] for i in range(len(S))]
+# Number of each type of each power production technology p to build at node n
+# integer
+xnp = [[0 for j in range(len(P))] for i in range(len(N))]
 
-# Number of each type of each power production technology p to build at node n, integer
-yhnsp = [[0 for j in range(len(P))] for i in range(len(S))]
+#  capacity of power production technology p used at node n
+# during operating mode h under scenario s
+# (MW)
+yhnsp = [[[[0 for l in range(len(P))] for k in range(S)] for j in range(len(N))] for i in range(H)]
 
 #loop over all possible well-formed solutions
     #If not feasible, next salution
@@ -46,5 +54,6 @@ yhnsp = [[0 for j in range(len(P))] for i in range(len(S))]
         #If value is smaller than stored value, store current value and solution
 #Return solution and objective value
 
-def evalObj(xnp):
+def evalObj(xnp, yhnsp, euhns):
+    print(P)
     print(xnp)
