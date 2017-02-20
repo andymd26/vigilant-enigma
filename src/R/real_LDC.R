5927ada8da334dcaf3040fc8a339ef296ef66e1b
Andrew Blohm <andymd26@Andrews-MacBook-Pro.local>
2017-02-20 08:37:29 -0500

    Working on techniques to approximate the load duration curve (LDC). We are exploring using regression trees, as well as an algorithm based on the idea of B-splines.

--- src/R/real_LDC.R
+++ src/R/real_LDC.R
@@ -1,9 +1,17 @@
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("rpart")
+install.packages("earth")
+install.packages("splines")
 # This file reads in real electricity system data for the year 2016 for the company PEPCO (a member of PJM). We use the data to create some realistic estimates for the toy model.
 require(dplyr)
+require(earth)
 require(ggplot2)
 require(rpart)
+require(splines)
 
 setwd("D:/Users/andymd26/Documents/vigilant-enigma/data")
+setwd("~/Documents/vigilant-enigma/data")
 file = "pepco_2016.txt"
 elec = scan(file, what="numeric", skip = 1)
 #scan reads in a matrix as a single vector. In this case data is stored by (rows=days, columns=hours). Instead we would like a single column vector.
@@ -32,4 +40,37 @@ ggplot() + geom_line(aes(x=dat$index,y=dat$elec))
 perc = 0.05
 threshold = dat[round(perc*8783), 'elec']
 
+function
+y = seq(1,nrow(dat),1)
+z = function(y){
+  a.mean = (dat[1:y,'elec'] - mean(dat[1:y,'elec']))^2
+}
+y.1 = sapply(sapply(y,z),sum)
 
+z = function(dat) {1:max(dat[,'index'])
+  
+}
+
+a = sapply(sapply(y, function(x) (dat[1:x,'elec'] - mean(dat[1:x,'elec']))^2 + 
+                    (dat[(x+1):max(x),'elec'] - mean(dat[(x+1):max(x),'elec']))^2), sum)
+
+a = sapply(y, function(x) mean(dat[(x+1):max(x),'elec']))
+
+
+a.mean = sapply(y, function(x) (dat[1:x,'elec'] - mean(dat[1:x,'elec']))^2) +
+                  (dat[(x+1):(max(y)-1),'elec'] - mean(dat[(x+1):(max(y)-1),'elec']))^2) 
+
+a.mean = sapply(sapply(y, function(x) (dat[1:x,'elec'] - mean(dat[1:x,'elec']))^2),sum)
+b.mean = sapply(sapply(y, function(x) (dat[(x+1):8783,'elec'] - mean(dat[(x+1):8783,'elec']))^2),sum)
+
+D=0
+K=5
+knots = nrow(dat) * (1:K)/(K+1)
+
+X1 = outer(dat$elec, 1:D,"^")
+X2 = outer(dat$elec, knots, ">") * outer(dat$elec, knots,"-")^D
+X = cbind(X1,X2)  
+  
+  
+  
+  
